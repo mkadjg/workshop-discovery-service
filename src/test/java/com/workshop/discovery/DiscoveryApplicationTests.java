bf6e3c8060099e147d93a19a3652aaee5720e5a5
mkadjg <sandi@Sandis-MacBook-Air.local>
2023-06-10 14:44:49 +0700

    inital commit

---
+++ src/test/java/com/workshop/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
